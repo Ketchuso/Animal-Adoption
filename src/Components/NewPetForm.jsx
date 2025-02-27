ab5b72ad8bd2db0c7ccbc2a100e5a9f70b47bf03
KerissaGit <kerissa525@gmail.com>
2025-02-27 12:11:05 -0500

    Updated on components till server update

--- src/Components/NewPetForm.jsx
+++ src/Components/NewPetForm.jsx
@@ -5,9 +5,18 @@ function NewPetForm() {
 
     
     return(
+        <div className="new-pet-form">
+        <h2>New Adoptable Pet</h2>
         <form>
-            
+          <input type="text" name="name" placeholder="Pet name" />
+          <input type="text" name="image" placeholder="Image URL" />
+          <input type="text" name="type" placeholder="Place holder Cat/Dog" />
+          <input type="text" name="age group" placeholder="Age group" />
+          <input type="text" name="temperment" placeholder="temperment" />
+          <input type="text" name="activities" placeholder="activities" />
+          <button type="submit">Add Pet</button>
         </form>
+      </div>
     )
 }
 
--- src/Components/PetContainer.jsx
+++ src/Components/PetContainer.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PetCards from "./PetCards";
 
 function PetContainer() {
+    const [pets, setPets] = useState([])
+
+    useEffect(() => {
+      fetch("http://localhost:3000/pets")
+      .then((resp) => resp.json())
+      .then((allPets) => setPets(allPets))
+    }, [])
+
+    const petCards = pets.map((pet) =>
+        <PetCards
+        key={pet.id} 
+        name={pet.name} 
+        image={pet.image}
+        />)
 
 
     return(
         <ul className="cards">
-            <PetCards />
+            {petCards}
         </ul>
     )
 }
