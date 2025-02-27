94bac7a4c5547aa439c644885df3104a4c0d73ab
KerissaGit <kerissa525@gmail.com>
2025-02-27 14:34:29 -0500

    Filtered pets up and running! Can view all pets, cats or dogs

--- src/Components/PetCards.jsx
+++ src/Components/PetCards.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 
-function PetCards({ name, image, type}) {
+function PetCards({ id, name, image, activities, ageGroup, temperament, adoptionStatus, viewPetClick}) {
 
-    
+    // function viewPetClick() {
+
+    // }
+
     return(
-        <li className="card">
+        <li className="card" >
             <img src={image} alt={name} />
             <h4>Hi! My name is: {name}</h4>
-            <h5>I'm a :{type}</h5>
+            <h5>Age Group: {ageGroup}</h5>
+            <p>Short Description: {temperament}</p>
+            <p>This Pet Enjoys: {activities}</p>
+            <p>Adoption Status: {adoptionStatus}</p>
+            {/* <button onClick={viewPetClick.id}>🐾View Pet!🐾</button> */}
         </li>
     )
 }
--- src/Components/PetContainer.jsx
+++ src/Components/PetContainer.jsx
@@ -3,25 +3,56 @@ import PetCards from "./PetCards";
 
 function PetContainer() {
     const [pets, setPets] = useState([])
+    const [petFilter, setPetFilter] = useState('all')
 
     useEffect(() => {
-      fetch("http://localhost:3000/pets")
+      fetch("http://localhost:5002/pets")
       .then((resp) => resp.json())
       .then((allPets) => setPets(allPets))
     }, [])
 
-    const petCards = pets.map((pet) =>
+
+    const filteredPets = pets.filter((pet) => {
+        if (petFilter === 'all') {
+            return true;
+        }
+        if (petFilter === 'cat' && pet.type === 'Cat') {
+            return true;
+        }
+        if (petFilter === 'dog' && pet.type === 'Dog') {
+            return true;
+        } return false;
+    });
+
+
+    const petCards = filteredPets.map((pet) =>
         <PetCards
         key={pet.id} 
         name={pet.name} 
         image={pet.image}
+        activities={pet.activities}
+        ageGroup={pet.age_group}
+        temperament={pet.temperament}
+        adoptionStatus={pet.adoption_status}
         />)
+
+        const handleFilterChange = (e) => {
+            setPetFilter(e.target.value)
+        }
 
 
     return(
-        <ul className="cards">
-            {petCards}
-        </ul>
+        <div>
+            <div>
+                <button value='all' onClick={handleFilterChange}> 🐾 View All Pets 🐾 </button>
+                <button value='cat' onClick={handleFilterChange}> 🐈 View All Cats 🐈 </button>
+                <button value='dog' onClick={handleFilterChange}> 🐕 View All Dogs 🐕 </button>
+            </div>
+
+            <ul className="cards">
+                {petCards}
+            </ul>
+        </div>
     )
 }
 
